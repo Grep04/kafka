4626fd559e3477e550064e68830944e56958a05a
NamSangwon <nsw3265@naver.com>
2025-12-31 14:22:25 +0900

    0003

---
+++ src/main/java/com/back/shared/event/MemberJoinedEvent.java
@@ -0,0 +1,7 @@
+package com.back.shared.event;
+
+import com.back.shared.dto.MemberDto;
+import com.back.standard.event.HasEventName;
+
+public record MemberJoinedEvent(MemberDto member) implements HasEventName {
+}
